2b27300b7e5023fc3fcfc69a3bf6d20f5a6f205b
Ruben Valdez <ruben.valdez7@gmail.com>
2019-08-07 17:47:23 -0700

    updated orm to boiler plate

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,11 +1,37 @@
 const burger = require("../models/burger")
 const express = require("express")
+const router = express.Router();
+const orm = require("../config/orm")
+//router
 
+router.get("/",function(req,res){
+      orm.selectAll(function(data){
+        var hbsObject = {
+          burger:data
+        };
+        console.log("router get " + hbsObject)
+        res.render("index", hbsObject)
+      });
+});
 
+router.post("/api/burgers", function(req,res){
+ 
+})
 
-
-
-
-
-
-module.exports = "router"
+router.put("/api/burger/:id",function(req,res){
+  var condition = "id = " +req.params.id;
+  console.log("condition", condition);
+  orm.updateOne(
+    {
+      devoured:req.body.sleepy
+    },
+    condition,
+    function(result){
+      if(result.changedRows === 0){
+        return res.status(404).end()
+      }
+      res.status(200).end()
+    }
+  );
+});
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,29 @@
-const express = require ("express");
-const handlebars = require("express-handlebars");
-const connection = require("./config/connection")
+var express = require("express");
 
-const app = express();
-const PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 8080;
 
-const orm = require("./config/orm")
+var app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-
-app.engine("handlebars", handlebars({ default:"main"}));
-app.set("view engine", "handlebars");
-
-app.use(express.urlencoded({ extended:true }));
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"))
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-var routes = require("./controllers/burgers_controller")
-// app.use(routes)
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.listen(PORT, function(){
-    console.log("App listening on PORT " + PORT);
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgerController.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
-
